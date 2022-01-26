e8331beb99efb7b1a96188b438405616bb3c55ea
Alfonso García <ajgarciaba@gmail.com>
2022-01-27 00:11:34 +0100

    Update next config

--- next.config.js
+++ next.config.js
@@ -15,20 +15,16 @@ const ContentSecurityPolicy = `
 `
 
 const securityHeaders = [
-  {
-    key: 'Access-Control-Allow-Origin',
-    value: 'https://*.disqus.com',
-  },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
   {
     key: 'Content-Security-Policy',
     value: ContentSecurityPolicy.replace(/\n/g, ''),
   },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-  {
-    key: 'Referrer-Policy',
-    value: 'strict-origin-when-cross-origin',
-  },
+  //{
+  //  key: 'Referrer-Policy',
+  //  value: 'strict-origin-when-cross-origin',
+  //},
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
   {
     key: 'X-Frame-Options',
