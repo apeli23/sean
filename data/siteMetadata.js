3222ae85e5adaabf21df4dd3a44f4cfcdd016a4a
Alfonso García <ajgarciaba@gmail.com>
2022-01-31 23:25:42 +0100

    Upload images

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -20,9 +20,6 @@ const siteMetadata = {
   locale: 'en-US',
   analytics: {
     // supports plausible, simpleAnalytics, umami or googleAnalytics
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    simpleAnalytics: false, // true or false
-    umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     googleAnalyticsId: 'G-2ZGMHVCTZX', // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
